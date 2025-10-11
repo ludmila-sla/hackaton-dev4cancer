68073a30b95c749d1e723169329fafcb59daf2b7
ludmila-sla <ludmilafroman@gmail.com>
2025-10-11 13:12:32 -0300

    correção

--- persistencia.py
+++ persistencia.py
@@ -1,9 +1,18 @@
-historico = []
+import uuid
 
-def responder(usuario_msg):
-    historico.append({"role": "user", "content": usuario_msg})
-    
-    resposta = gerar_resposta(historico)
-    
-    historico.append({"role": "assistant", "content": resposta})
+def criar_session():
+    return str(uuid.uuid4())
+
+conversas = {}
+
+def responder(session_id, user_msg):
+    if session_id not in conversas:
+        conversas[session_id] = []
+
+    conversas[session_id].append({"role": "user", "content": user_msg})
+
+    resposta = gerar_resposta(conversas[session_id])
+
+    conversas[session_id].append({"role": "assistant", "content": resposta})
+
     return resposta
